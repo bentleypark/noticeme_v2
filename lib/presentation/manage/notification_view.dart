73bac6b3953592ba915b55c752b7646aa595607b
bentleypark <p2c2kbf@gmail.com>
2021-10-13 08:18:49 +0900

    autoSizeText 적용

--- lib/presentation/manage/notification_view.dart
+++ lib/presentation/manage/notification_view.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:noticeme/resources/colors.dart';
 import 'package:noticeme/utils/ex_fuctions.dart';
@@ -5,40 +6,44 @@ import 'package:noticeme/utils/ex_fuctions.dart';
 class NotificationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return SizedBox(
       child: Card(
         elevation: 1,
         margin: EdgeInsets.fromLTRB(8, 0, 8, 24),
-        child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Padding(
-            padding: EdgeInsets.only(left: 6, right: 8),
-            child: Icon(
-              Icons.close,
-              color: COLOR_GRAY01.parseColor(),
-            ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: EdgeInsets.only( right: 8),
-              child: Text(
-                '최대일곱자가능 바꾸는 날이에요',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 6, right: 8),
+                child: Icon(
+                  Icons.close,
+                  color: COLOR_GRAY01.parseColor(),
+                ),
               ),
-            ),
-          ),
-          OutlinedButton(
-              onPressed: update,
-              child: Text('교체하기'),
-              style: OutlinedButton.styleFrom(
-                  textStyle: TextStyle(
-                      color: COLOR_GREEN_ORANGE.parseColor(),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700),
-                  side: BorderSide(
-                      color: COLOR_GREEN_ORANGE.parseColor(), width: 1),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0))))
-        ]),
+              Flexible(
+                  child:
+                  Padding(
+                padding: EdgeInsets.only(right: 8),
+                child: AutoSizeText(
+                  '최대일곱자가능 바꾸는 날이에요',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
+                  maxLines: 1,
+                ),
+              ),),
+              OutlinedButton(
+                  onPressed: update,
+                  child: Text('교체하기'),
+                  style: OutlinedButton.styleFrom(
+                      textStyle: TextStyle(
+                          color: COLOR_GREEN_ORANGE.parseColor(),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700),
+                      side: BorderSide(
+                          color: COLOR_GREEN_ORANGE.parseColor(), width: 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0))))
+            ]),
       ),
     );
   }
